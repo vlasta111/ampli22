1ad80f087410076dd23f3747d12fc7c5da8ca01d
vlasta111 <72654520+vlasta111@users.noreply.github.com>
2021-01-29 23:37:08 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+
+const block = document.querySelector('.block');
+const button = document.querySelector('#button');
+
+button.addEventListener('click', function(){
+  block.classList.toggle('changeBlock');
+})
+
+const block2 = document.querySelector('.block2');
+const button2 = document.querySelector('#button2');
+
+button2.addEventListener('click', function(){
+  block2.classList.toggle('changeBlock2');
+})
+
+const block3 = document.querySelector('.block3');
+const button3 = document.querySelector('#button3');
+
+button3.addEventListener('click', function(){
+  block3.classList.toggle('changeBlock3');
+})
+
+
+
+
+
+  
+
+
